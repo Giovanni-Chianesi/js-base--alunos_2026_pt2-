11d85b89c765668256441b4d61151be0a39894fa
gioonezi <giovannichianesi@gmail.com>
2026-03-12 09:35:59 -0300

    commit ex10 100% feito finalmente

--- ex10-menu-hamburguer/script.js
+++ ex10-menu-hamburguer/script.js
@@ -1,13 +1,5 @@
-// Seleciona o ícone do menu e a lista de navegação pelo ID
-const menuBtn = document.getElementById('menuBtn');
-const menu = document.getElementById('menu');
-
-menuBtn.addEventListener('click', () => {
-  menu.classList.toggle('active');
-});
-
-// Adiciona um evento de clique ao ícone do menu
-menuIcon.addEventListener('click', () => {
-  // Alterna a classe "active" na lista de navegação
-  navList.classList.toggle('active');
-});
+const menuBtn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("active");
+}); 
